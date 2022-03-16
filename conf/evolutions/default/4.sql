e833aa74835a38c661289f1ef3da3dff1cf57af6
VoltG3 <kristians.kodehode@gmail.com>
2022-03-16 10:13:23 +0100

    to delete

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -3,7 +3,7 @@
 
 # --- !Ups
 
-insert into department (id,name,created_at,company_id) values (default,'unknown department 1','1983-06-27',1);
+insert into department (id,name,created_at,company_id) values (default,'unknown department 1',now(),1);
 insert into department (id,name,created_at,company_id) values (default,'unknown department 2',now(),2);
 
 
